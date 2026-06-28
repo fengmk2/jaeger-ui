8076976288cba83a70bd67007a060939ee88a97e
MK <fengmk2@gmail.com>
2026-06-28 23:59:32 +0800

    vp migrate beta test

--- packages/jaeger-ui/src/vite-env.d.ts
+++ packages/jaeger-ui/src/vite-env.d.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2019 Uber Technologies, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-/// <reference types="vite/client" />
+/// <reference types="vite-plus/client" />
 
 interface ImportMetaEnv {
   readonly VITE_JAEGER_AG_UI_URL?: string;
